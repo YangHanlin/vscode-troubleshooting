c68aa9e6ba380a03d760b4c36879ce5ddb5f122d
YangHanlin <mattoncis@hotmail.com>
2019-06-20 17:08:01 +0800

    Initial commit

---
+++ hello-world.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "hello, world" << std::endl;
+    system("pause"); // I added this to make the program pause so that I can see the output
+    return 0;
+}
